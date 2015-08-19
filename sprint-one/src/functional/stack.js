69e608e071ff65e962d6d3d6e909fdaee0076671
Tomio Mizoroki <tmizoroki@gmail.com>
2015-08-19 14:58:16 -0700

    Get functional stack tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,22 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+  var keyArr = Object.keys(storage);
+  var nextKey = keyArr.length === 0 ? 0 : keyArr[keyArr.length - 1] + 1;
+  storage[nextKey] = value;
   };
 
   someInstance.pop = function(){
+  var keyArr = Object.keys(storage);
+  var lastKey = keyArr[keyArr.length - 1];
+  var temp = storage[lastKey];
+  delete storage[lastKey];
+  return temp;
   };
 
   someInstance.size = function(){
+    var keyArr = Object.keys(storage);
+    return keyArr.length;
   };
 
   return someInstance;
