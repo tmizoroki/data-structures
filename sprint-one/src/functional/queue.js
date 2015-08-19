ee2d7eb0b2b51cc9b8f5724176aa8899baec9b32
tmizoroki <tmizoroki@gmail.com>
2015-08-19 16:00:28 -0700

    All tests passing on functional queue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,35 @@
 var Queue = function(){
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
+      
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    if (Object.keys(storage).length === 0) {
+      debugger;
+      storage[0] = value;
+    } else {
+      for (var key in storage) {
+        var numKey = Number(key) + 1;
+        storage[numKey] = storage[key];
+      }  
+      storage[0] = value;
+    }
   };
 
   someInstance.dequeue = function(){
+    var keyArr = Object.keys(storage);
+    var lastKey = keyArr[keyArr.length - 1];
+    var temp = storage[lastKey];
+    delete storage[lastKey];
+    return temp;
   };
 
   someInstance.size = function(){
+    var keyArr = Object.keys(storage);
+    return keyArr.length;
   };
 
   return someInstance;
