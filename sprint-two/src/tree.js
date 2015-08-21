907d5e56e98a6a27607ae7bf7edc962eaf989168
tmizoroki <tmizoroki@gmail.com>
2015-08-20 17:49:16 -0700

    Implement tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,26 +1,55 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
 
   // your code here
-  newTree.children = null;  // fix me
+  //newTree.children = null;  // fix me
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
+  if (this.value === target) {
+    return true;
+  }
 
+
+var bool = false;
+
+  var deepSearch = function(node) {
+    if(node.value === target){
+      bool = true;
+    }
+
+    if(node.children.length > 0){
+      
+      _.each(node.children, function(item, i) {
+        if (item.value === target) {
+          bool = true;
+        }
+      });
+
+    }
+   
+  };
+  
+  for(var i =0; i<this.children.length; i++) {
+    deepSearch(this.children[i]);
+  }
+
+  return bool;
 };
 
 
