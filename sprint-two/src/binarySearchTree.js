c775ceacb95d50b8141c26e8d411b5ce214f06ec
tmizoroki <tmizoroki@gmail.com>
2015-08-21 23:29:59 -0700

    Implement binary search trees

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,60 @@
 var BinarySearchTree = function(value){
+  var tree = Object.create(BinarySearchTree.prototype);
+  tree.left = null;
+  tree.right = null;
+  tree.value = value;
+  return tree;
 };
 
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+BinarySearchTree.prototype.insert = function(value){
+  
+  if (value < this.value) {
+    if (!this.left) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else if (value > this.value) {
+    if (!this.right) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+}
+
+BinarySearchTree.prototype.contains = function(value){
+  if (this.value === value) {
+    return true;
+  } else if (this.value < value && this.right) {
+    return this.right.contains(value);
+  } else if (this.value > value && this.left) {
+    return this.left.contains(value);
+  }
+  return false;
+}
+
+
+BinarySearchTree.prototype.depthFirstLog = function(callback){
+
+if(this.value){
+  callback(this.value);
+}
+
+if (this.right){
+  this.right.depthFirstLog(callback);
+}
+
+if (this.left){
+  this.left.depthFirstLog(callback);
+}
+
+
+};
+
+ 
+
+
+
+
